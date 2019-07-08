564c0200815d948eeb3a3200fc992ca28cbe2b1b
pedro <gaopedro@163.com>
2019-07-08 10:05:39 +0800

    fix:修复AuthInterceptor在无RouteMeta修饰情况下的权限认证

--- lin-cms-spring-boot-autoconfigure/src/main/java/com/lin/cms/interceptor/AuthInterceptor.java
+++ lin-cms-spring-boot-autoconfigure/src/main/java/com/lin/cms/interceptor/AuthInterceptor.java
@@ -37,52 +37,71 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             Method method = handlerMethod.getMethod();
             RouteMeta meta = method.getAnnotation(RouteMeta.class);
-
-            // 如果视图函数没有 RouteMeta 注解，那么视图函数没有被加入到权限控制中
-            // 因此，即使被 LoginRequired 等注解装饰，也不会被权限校验
-            // TODO: 考虑meta的存在与否
+            // 考虑两种情况，1. 有 meta；2. 无 meta
+            // 无meta的情况下，可以使用 LoginRequired 和 AdminRequired 修饰
+            // 有meta的情况则走以下逻辑
             if (meta == null) {
-                return true;
-            }
-
-            // 没有挂载到权限系统中，通过
-            if (!meta.mount()) {
-                return true;
-            }
-
-            String methodName = method.getName();
-            String className = method.getDeclaringClass().getName();
-            Object meta1 = postProcessor.getMetaMap().get(className + "#" + methodName);
-
-            // 如果已经挂载，且二者相同
-            if (meta == meta1) {
-                Annotation[] annotations = method.getAnnotations();
-                UserLevel level = AnnotationHelper.findRequired(annotations);
-                switch (level) {
-                    case TOURIST:
-                        // 如果，当前视图函数是游客权限，则直接返回true
-                        return true;
-                    case LOGIN:
-                        // 登陆权限
-                        // 必须是登陆权限
-                        boolean valid = authInterceptorResolver.verifyLogin(request, response, result, meta);
-                        return valid;
-                    case GROUP:
-                        boolean valid1 = authInterceptorResolver.verifyGroup(request, response, result, meta);
-                        return valid1;
-                    case ADMIN:
-                        boolean valid2 = authInterceptorResolver.verifyAdmin(request, response, result, meta);
-                        return valid2;
-                    case REFRESH:
-                        boolean valid3 = authInterceptorResolver.verifyRefresh(request, response, result, meta);
-                        return valid3;
-                    default:
-                        ResultGenerator.writeResult(response, result);
-                        return false;
-                }
+                return this.handleNoMeta(request, response, result, method);
             } else {
-                ResultGenerator.writeResult(response, result);
+                return this.handleMeta(request, response, result, method, meta);
+            }
+        } else {
+            // 不是视图函数，则当做失败处理
+            ResultGenerator.writeResult(response, result);
+            return false;
+        }
+    }
+
+    private boolean handleNoMeta(HttpServletRequest request, HttpServletResponse response, Result result, Method method) {
+        Annotation[] annotations = method.getAnnotations();
+        UserLevel level = AnnotationHelper.findRequired(annotations);
+        switch (level) {
+            case TOURIST:
+                return true;
+            case LOGIN:
+                return authInterceptorResolver.verifyLogin(request, response, result, null);
+            case ADMIN:
+                return authInterceptorResolver.verifyAdmin(request, response, result, null);
+            case REFRESH:
+                return authInterceptorResolver.verifyRefresh(request, response, result, null);
+            default:
                 return false;
+        }
+    }
+
+    private boolean handleMeta(HttpServletRequest request, HttpServletResponse response, Result result, Method method, RouteMeta meta) {
+        // 没有挂载到权限系统中，通过
+        if (!meta.mount()) {
+            return true;
+        }
+        String methodName = method.getName();
+        String className = method.getDeclaringClass().getName();
+        Object meta1 = postProcessor.getMetaMap().get(className + "#" + methodName);
+        // 如果已经挂载，且二者相同
+        if (meta == meta1) {
+            Annotation[] annotations = method.getAnnotations();
+            UserLevel level = AnnotationHelper.findRequired(annotations);
+            switch (level) {
+                case TOURIST:
+                    // 如果，当前视图函数是游客权限，则直接返回true
+                    return true;
+                case LOGIN:
+                    // 登陆权限
+                    // 必须是登陆权限
+                    boolean valid = authInterceptorResolver.verifyLogin(request, response, result, meta);
+                    return valid;
+                case GROUP:
+                    boolean valid1 = authInterceptorResolver.verifyGroup(request, response, result, meta);
+                    return valid1;
+                case ADMIN:
+                    boolean valid2 = authInterceptorResolver.verifyAdmin(request, response, result, meta);
+                    return valid2;
+                case REFRESH:
+                    boolean valid3 = authInterceptorResolver.verifyRefresh(request, response, result, meta);
+                    return valid3;
+                default:
+                    ResultGenerator.writeResult(response, result);
+                    return false;
             }
         } else {
             ResultGenerator.writeResult(response, result);
